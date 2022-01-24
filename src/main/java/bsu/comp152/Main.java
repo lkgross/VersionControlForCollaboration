e48b5c6cad3ac457e014118a357eb1cbb159a730
zacharesmer <zachresmer@gmail.com>
2022-01-23 20:19:31 -0500

    Initial commit

---
+++ src/main/java/bsu/comp152/Main.java
@@ -0,0 +1,9 @@
+package bsu.comp152;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("These are some fun facts:");
+        // TODO: Add your own fun fact and print it after the existing one(s)
+        System.out.println("Four bits is half of a byte and is called a nybble");
+    }
+}
